7ebc9f4d74c293e05629daaeca31cf8507f119b8
Mathis Mahaux <mathis.mahaux@gmail.com>
2024-12-04 20:43:36 +0100

    User has access to certain parts of the site depending on their role

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,24 @@
 import { Routes } from '@angular/router';
+import {RoleGuard} from './guards/role.guard';
+import {NotAuthorizedComponent} from './components/not-authorized/not-authorized.component';
+import {UserComponent} from './components/user/user.component';
+import {AdminComponent} from './components/admin/admin.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: 'admin',
+    component: AdminComponent,
+    canActivate: [RoleGuard],
+    data: { roles: ['admin'] }
+  },
+  {
+    path: 'user',
+    component: UserComponent,
+    canActivate: [RoleGuard],
+    data: { roles: ['admin', 'user'] }
+  },
+  {
+    path: 'not-authorized',
+    component: NotAuthorizedComponent
+  }
+];
